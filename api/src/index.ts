39a4ece75f2643dee8240599ff126fdf4923e0ac
John Doe <asgudev@gmail.com>
2025-12-23 14:16:33 +0300

    Добавлен x-server-host в CORS allowedHeaders

--- api/src/index.ts
+++ api/src/index.ts
@@ -38,7 +38,7 @@ app.use(cors({
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-server-host'],
   credentials: true,
 }));
 
